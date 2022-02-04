aa2f887b6c5687b4ca9b74444dae34edc14bbe52
mk <kosminskimateusz@gmail.com>
2022-02-04 12:56:20 +0100

    Comment in calculate.js TODO next

--- js/functions/calculate.js
+++ js/functions/calculate.js
@@ -3,9 +3,14 @@ import { Room } from "../sources/Room.js";
 export function calculate() {
   const rooms = document.querySelectorAll(".room");
   let roomsObject = new Array();
-  let id = 1;
+  let id = 0;
+  // const temperatures = collect array from inputs
+  // const outsideTemperature = get from last array record
+  // cut temperatures last record
+  const temperature = 20;
+
   for (let el of rooms) {
-    roomsObject.push(new Room(id));
+    roomsObject.push(new Room(id, temperature));
     id++;
   }
   console.log(roomsObject);
@@ -21,11 +26,11 @@ export function calculate() {
         diffX = room.x - r.x;
         diffY = room.y - r.y;
         // console.log("diffX: ", diffX);
-        if (-200 < diffX < 200 && diffY === 0) {
+        if ((-200 < diffX < 200) && diffY === 0) {
           diffX < 0 ? room.right = r : null;
           diffX > 0 ? room.left = r : null;
         }
-        if (-200 < diffY < 200 && diffX === 0) {
+        if ((-200 < diffY < 200) && diffX === 0) {
           diffY < 0 ? room.bottom = r : null;
           diffY > 0 ? room.top = r : null;
         }
--- js/sources/Room.js
+++ js/sources/Room.js
@@ -6,11 +6,13 @@ export class Room {
   right;
   top;
   bottom;
+  temperature = 20;
 
-  constructor(id) {
+  constructor(id, temperature) {
     this.id = id;
     this.x = localStorage.getItem("x" + id);
     this.y = localStorage.getItem("y" + id);
+    this.temperature = temperature;
   }
 
 }
